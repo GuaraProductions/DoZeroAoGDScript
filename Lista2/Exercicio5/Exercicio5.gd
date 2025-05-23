71067c4b214e29f98d9ffb0cf27fc0fa215e1433
Gustavo Lopes <gustavolr035@gmail.com>
2025-05-23 12:51:30 -0300

    Feito lista 2 de exercicios

---
+++ Lista2/Exercicio5/Exercicio5.gd
@@ -0,0 +1,28 @@
+extends Node
+
+#region JahImplementado
+
+@onready var entrada = $CampoNumero
+@onready var resultado = $LabelResultado
+
+func _eh_numero_valido(texto: String) -> bool:
+	return texto.is_valid_int()
+
+func calcular():
+	var texto = entrada.text
+
+	if not _eh_numero_valido(texto):
+		resultado.text = "Erro! A entrada de texto é inválida!\nColoque um número inteiro!"
+		resultado.modulate = Color.RED
+		return
+
+	var n = int(texto)
+	var soma = somar_pares(n)
+	resultado.modulate = Color.WHITE
+	resultado.text = "Soma dos pares: %d" % soma
+
+#endregion
+
+func somar_pares(n: int) -> int:
+	# TODO: Some apenas os números pares entre 1 e n
+	return 0
